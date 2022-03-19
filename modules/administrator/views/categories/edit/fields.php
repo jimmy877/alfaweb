f01f2f82d64cea2025cae4d8b2087728ab9d82c0
jimmy877 <molot877@mail.ru>
2022-03-19 13:52:53 +0300

    поставил условие по выводу группы полей или редактирвоания конкретных полей, если уже была ранее привязана группа

--- modules/administrator/views/categories/edit/fields.php
+++ modules/administrator/views/categories/edit/fields.php
@@ -1,17 +1,38 @@
-<?if(!defined("adm")) die;?>
+<?php if(!defined("adm")) die;?>
 <div class="tabs tab_fields">
     <article class="module width_3_quarter myclassModule" id="target">
         <header>
             <h3 class="tabs_involved"><?=$category->title?></h3>
         </header>
 
+
         <!-- тут контент для вывода доп полей из прототипа-->
         <div class="editCat">
-            <? var_dump($fieldsPrototypeCategory);?>
-            <?php foreach ($fieldsPrototypeCategory as $group):?>
-                <div> <?php echo $group['group_name']?> </div>
-            <?php endforeach ?>
+
+            <!-- если категория ранее не была привязана, выводим список категорий полей -->
+            <?php if ($category->fieldGroupID == 0): ?>
+                <?php var_dump($fieldsPrototypeCategory);?>
+                <select>
+                    <option selected value="null"> Выбрать категорию</option>
+                    <?php foreach ($fieldsPrototypeCategory as $group):?>
+                        <option value="<?php echo $group['id']?>" >
+                            <?php echo $group['group_name']?>
+                        </option>
+                    <?php endforeach ?>
+                </select>
+            <?php else:?>
+                Вывод полей
+            <?php endif;?>
+
+            <?php
+                /***
+                 * надо сделать ajax функцию через axios и с использованием vuejs для подгрузки нужный полей тут, согласно
+                 * выбранной категории
+                 */
+            ?>
         </div>
+
+
 
     </article>
 
