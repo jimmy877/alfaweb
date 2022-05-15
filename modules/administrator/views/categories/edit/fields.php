9961bd636630496c4a52f57a01802640055633c0
jimmy877 <molot877@mail.ru>
2022-05-15 11:34:39 +0300

    поставил условие по выводу группы полей или редактирвоания конкретных полей, если уже была ранее привязана группа

--- modules/administrator/views/categories/edit/fields.php
+++ modules/administrator/views/categories/edit/fields.php
@@ -20,6 +20,8 @@
                         </option>
                     <?php endforeach ?>
                 </select>
+                <a class="ac_button" href="/administrator/categories/edit?id=<?php echo $category->id?>&field=<?php echo $category->id?>">Выбрать</a>
+
             <?php else:?>
                 Вывод полей
             <?php endif;?>
